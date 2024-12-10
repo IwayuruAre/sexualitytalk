077e5c24cf13a794137a125419c4d80f44cf8a4d
s.tokutake <s.tokutake2458@gmail.com>
2024-12-10 21:30:44 +0900

    self review:ユーザーエンティティのアクセサ見直し

--- core/domain/entities/user.ts
+++ core/domain/entities/user.ts
@@ -1,82 +1,34 @@
 import { UserOptions } from "./userOptions";
 
 export class User {
-    private readonly _id: string;
-    private readonly _birth: Date;
-    private readonly _name: string;
-    private readonly _sex: string;
-    private _chatRooms: string[];
-    private _lgbt: string[];
-    private _photos: string[];
-    private _prefecture: string;
-    private _profile: string;
-    private _createdAt: Date | null;
-    private _updatedAt: Date | null;
+    public readonly chatRooms: string[];
+    public readonly photos: string[];
+    public readonly profile: string;
+    public readonly createdAt: Date | null;
+    public readonly updatedAt: Date | null;
 
     constructor(
-        id: string,
-        name: string,
-        birth: Date,
-        sex: string,
-        lgbt: string[],
-        prefecture: string,
+        public readonly id: string,
+        public readonly name: string,
+        public readonly birth: Date,
+        public readonly sex: string,
+        public readonly lgbt: string[],
+        public readonly prefecture: string,
         options: UserOptions = {}
     ) {
-        this._id = id;
-        this._birth = birth;
-        this._lgbt = lgbt;
-        this._name = name;
-        this._sex = sex;
-        this._prefecture = prefecture;
-        this._chatRooms = options?.chatRooms || [];
-        this._photos = options?.photos || [];
-        this._profile = options?.profile || "";
-        this._createdAt = options?.createdAt || null;
-        this._updatedAt = options?.updatedAt || null;
+        this.chatRooms = options?.chatRooms || [];
+        this.photos = options?.photos || [];
+        this.profile = options?.profile || "";
+        this.createdAt = options?.createdAt || null;
+        this.updatedAt = options?.updatedAt || null;
         this.validate();
-    }
-
-    get id(): string {
-        return this._id;
-    }
-
-    get birth(): Date {
-        return this._birth;
-    }
-
-    get chatRooms(): string[] {
-        return this._chatRooms;
-    }
-
-    get lgbt(): string[] {
-        return this._lgbt;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    get photos(): string[] {
-        return this._photos;
-    }
-
-    get prefecture(): string {
-        return this._prefecture;
-    }
-
-    get profile(): string {
-        return this._profile;
-    }
-
-    get sex(): string {
-        return this._sex;
     }
 
     get age(): number {
         const today = new Date();
-        let age = today.getFullYear() - this._birth.getFullYear();
-        const birthMonth = this._birth.getMonth();
-        const birthDay = this._birth.getDate();
+        let age = today.getFullYear() - this.birth.getFullYear();
+        const birthMonth = this.birth.getMonth();
+        const birthDay = this.birth.getDate();
 
         if (
             today.getMonth() < birthMonth ||
@@ -86,32 +38,6 @@ export class User {
         }
         return age;
     }
-
-    get createdAt(): Date | null {
-        return this._createdAt;
-    }
-
-    get updatedAt(): Date | null {
-        return this._updatedAt;
-    }
-
-    //#region  編集可能フィールド
-    public updateProfile(profile: string): void {
-        this._profile = profile;
-    }
-
-    public updatePrefecture(prefecture: string): void {
-        this._prefecture = prefecture;
-    }
-
-    public updateLGBT(lgbt: string[]): void {
-        this._lgbt = lgbt;
-    }
-
-    public updatePhotos(photos: string[]): void {
-        this._photos = photos;
-    }
-    //#endregion  編集可能フィールド
 
     private validate(): void {
         if (this.id === "") {
